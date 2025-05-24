5720d22135b761d8bbc0cdc2ddf588be21c1db06
Redfly54 <104248562+Redfly54@users.noreply.github.com>
2025-05-24 07:03:01 +0700

    change docker

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import flowbiteReact from "flowbite-react/plugin/vite";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   server: {
     host: true,
-    port: 9000,
+    port: 5173,
     watch: {
       usePolling: true,
     },
