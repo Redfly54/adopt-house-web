dedfc5cefba617b00b3032067fb3872514799a1c
Redfly54 <104248562+Redfly54@users.noreply.github.com>
2025-05-24 07:44:11 +0700

    change docker compose

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
     watch: {
       usePolling: true,
